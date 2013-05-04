ebaba7bf39624779a0a496d5d41bae619971f531
Robin Chou <chourobin@gmail.com>
2013-05-04 13:15:52 -0400

    initialized app

--- Navigator/ViewController.m
+++ Navigator/ViewController.m
@@ -7,23 +7,48 @@
 //
 
 #import "ViewController.h"
+#import <MapKit/MapKit.h>
 
-@interface ViewController ()
+@interface ViewController () <MKMapViewDelegate>
+
+@property (strong, nonatomic) NSDictionary *userLocation;
 
 @end
 
-@implementation ViewController
+@implementation ViewController 
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    self.mapView.delegate = self;
+    [self.mapView setShowsUserLocation:YES];
+    [self moveMapToNYC];
+    [SVProgressHUD showWithStatus:@"Loading..." maskType:SVProgressHUDMaskTypeGradient];
 }
 
 - (void)didReceiveMemoryWarning
 {
     [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    _userLocation = nil;
+}
+
+#pragma mark - Custom Actions
+
+-(void)moveMapToNYC
+{
+    CLLocationCoordinate2D sf = CLLocationCoordinate2DMake(40.734381,-73.992748);
+    MKCoordinateSpan span = MKCoordinateSpanMake(0.040, 0.040);
+    MKCoordinateRegion region = MKCoordinateRegionMake(sf, span);
+    [self.mapView setRegion:region];
+}
+
+#pragma mark - MapView Delegate
+
+- (void)mapView:(MKMapView *)mapView didUpdateUserLocation:(MKUserLocation *)userLocation
+{
+    self.userLocation = @{@"lat":[NSNumber numberWithDouble:userLocation.coordinate.latitude], @"lng":[NSNumber numberWithDouble:userLocation.coordinate.longitude]};
+    NSLog(@"%@", self.userLocation);
+    [SVProgressHUD dismiss];
 }
 
 @end
