26105602ec77f6f0197bf33b22e0256b740987f0
Robin Chou <chourobin@gmail.com>
2013-05-04 12:41:24 -0400

    Initial Commit

---
+++ Navigator/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Navigator
+//
+//  Created by Robin Chou on 5/4/13.
+//  Copyright (c) 2013 Robin Chou. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ Navigator/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Navigator
+//
+//  Created by Robin Chou on 5/4/13.
+//  Copyright (c) 2013 Robin Chou. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
