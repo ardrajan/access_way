a3468122bf9038b2f75fe6646a614458aa1a5ded
Robin Chou <chourobin@gmail.com>
2013-07-20 12:33:18 -0400

    Readded node-gtfs service

--- Node/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongo_url: process.env.MONGOHQ_URL || 'mongodb://localhost:27017/gtfs',
-  agencies: [
-    /* Put agency_key names from gtfs-data-exchange.com.
-    Optionally, specify a download URL to use a dataset not from gtfs-data-exchange.com */
-    'nyct', { agency_key: 'nyct', url: 'http://www.mta.info/developers/data/nyct/subway/google_transit.zip'}
-  ]
-};
