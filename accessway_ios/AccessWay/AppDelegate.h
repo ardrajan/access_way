ba6fd541d1cb19c58b9ee7c7c9bf2d9deb36e17f
ardrajan <ard.rajan@gmail.com>
2013-08-04 14:33:45 -0400

    First Commit

--- accessway_ios/AccessWay/AppDelegate.h
+++ accessway_ios/AccessWay/AppDelegate.h
@@ -5,6 +5,8 @@
 //  Created by Robin Chou on 5/4/13.
 //  Copyright (c) 2013 Robin Chou. All rights reserved.
 //  Dharmarajan Ayakkad Version
+//  Created by Robin Chou and Dharmrajan Ayakkad on 5/4/13.
+//  Copyright (c) 2013 Dharmarajan Ayakkad, Kristin Loeb, Robin Chou. All rights reserved.
 
 #import <UIKit/UIKit.h>
 
